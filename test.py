47ed3ecd6e73bfa080c86e0d2dd1332b0640fc8a
Seungwoo Yoo <rufjdk5480@gmail.com>
2023-12-22 13:27:28 +0900

    temp complete talkto

--- test.py
+++ test.py
@@ -1,45 +1,92 @@
+import os
 import sqlite3
 import typer
 import torch
 from transformers import BitsAndBytesConfig, AutoModelForCausalLM, AutoTokenizer, TextStreamer
 from peft import PeftModel, PeftModelForCausalLM
+from transformers.utils.logging import set_verbosity_error, disable_progress_bar
+import warnings
 
 
 app = typer.Typer()
 
-MODEL = None
-TOKENIZER = None
+MODEL_MANAGER = None
+
+class ModelManager:
+    def __init__(self, base_model_id):
+        self.conversation_history = []
+
+        # Configure BitsAndBytes
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_compute_dtype=torch.bfloat16
+        )
+
+        # Load model
+        self.model = AutoModelForCausalLM.from_pretrained(
+            base_model_id,
+            quantization_config=bnb_config,
+            trust_remote_code=True,
+            use_auth_token=True
+            )
+
+        # Load tokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained(base_model_id, add_bos_token=True, trust_remote_code=True)
+
+    def unload_model(self):
+        """
+        Unloads the model and tokenizer to free up resources.
+        """
+        # Clear model from GPU memory
+        if self.model is not None:
+            del self.model
+            torch.cuda.empty_cache()  # Clear cached memory
+
+        # Clear tokenizer
+        if self.tokenizer is not None:
+            del self.tokenizer
+
+        # Optionally, you can also set these to None to break any references
+        self.model = None
+        self.tokenizer = None
+
+    def talk_to(self):
+        self.model.eval()
+
+        while True:
+            eval_prompt = typer.prompt("\nuser ")
+            if eval_prompt == "!bye":
+                self.unload_model()
+                break
+            elif eval_prompt == "!history":
+                print(self.conversation_history)
+            else:
+                self.conversation_history.append({"role": "user", "content": eval_prompt})
+                model_input = self.tokenizer.apply_chat_template(self.conversation_history[:5], return_tensors="pt").to("cuda")
+
+                num_input_tokens = model_input.shape[1]
+
+                streamer = TextStreamer(self.tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+                generated_tokens = self.model.generate(model_input, 
+                                                    streamer=streamer, 
+                                                    max_new_tokens=100, 
+                                                    repetition_penalty=1.15)
+
+                output_tokens = generated_tokens[:, num_input_tokens:]
+                assistant_response = self.tokenizer.decode(output_tokens[0], skip_special_tokens=True)
+
+                # Add assistant response to conversation history
+                self.conversation_history.append({"role": "assistant", "content": assistant_response})
 
 
 @app.command()
-def load_model(base_model_id):
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_compute_dtype=torch.bfloat16
-    )
+def run(base_model_id):
+    global MODEL_MANAGER
+    MODEL_MANAGER = ModelManager(base_model_id)
+    MODEL_MANAGER.talk_to()
 
-    global MODEL 
-    MODEL = AutoModelForCausalLM.from_pretrained(
-        base_model_id,
-        quantization_config=bnb_config,
-        device_map="auto",
-        trust_remote_code=True,
-        use_auth_token=True
-    )
-
-    global TOKENIZER
-    TOKENIZER = AutoTokenizer.from_pretrained(base_model_id, add_bos_token=True, trust_remote_code=True)
-
-def talk_to(eval_prompt):
-    global MODEL
-    global TOKENIZER
-    
-    MODEL.eval()
-    model_input = TOKENIZER(eval_prompt, return_tensors="pt").to("cuda")
-    streamer = TextStreamer(TOKENIZER, skip_prompt=True)
-
-    _ = MODEL.generate(**model_input, streamer=streamer, max_new_tokens=30, repetition_penalty=1.15)
 
 # File path for the SQLite database
 db_file = 'my_database.db'
@@ -98,29 +145,11 @@ def list_models():
     else:
         typer.echo("No models found.")
 
-
-@app.command()
-def start():
-    initialize_db()
-    typer.echo("Starting CLI app with SQLite file-based database")
-    while True:
-        command = typer.prompt("Enter command (add_model, list_models, check_current_model, load_model, or exit):")
-
-        if command.lower() == 'exit':
-            typer.echo("Exiting the application.")
-            break
-        elif command.lower() == 'add_model':
-            name = typer.prompt("Enter name")
-            add_model(name)
-        elif command.lower() == 'list_models':
-            list_models()
-        elif command.lower() == 'load_model':
-            name = typer.prompt("Enter name")
-            load_model(name)
-        elif command.lower() == 'talk_to':
-            prompt = typer.prompt("Enter prompt")
-            talk_to(prompt)
-
-
 if __name__ == "__main__":
+    initialize_db()
+
+    warnings.filterwarnings('ignore', category=FutureWarning)
+
+    set_verbosity_error()
+    disable_progress_bar()
     app()
