162ed68d886bae37f2dadffe60b91f5f14ae09b0
Seungwoo Yoo <rufjdk5480@gmail.com>
2023-12-22 15:45:48 +0900

    temp

--- test.py
+++ test.py
@@ -4,16 +4,25 @@ import typer
 import torch
 from transformers import BitsAndBytesConfig, AutoModelForCausalLM, AutoTokenizer, TextStreamer
 from peft import PeftModel, PeftModelForCausalLM
+from huggingface_hub import try_to_load_from_cache
 from transformers.utils.logging import set_verbosity_error, disable_progress_bar
 import warnings
+from rich import print
+from rich.console import Console
+from rich.table import Table
+
+from utils import ascii_art
 
 
 app = typer.Typer()
-
+db_file = 'my_database.db'
 MODEL_MANAGER = None
 
 class ModelManager:
     def __init__(self, base_model_id):
+        print(f"[bold blue]{ascii_art}[/bold blue]")
+        print(f"[bold red]loading...{base_model_id}[/bold red]")
+
         self.conversation_history = []
 
         # Configure BitsAndBytes
@@ -55,19 +64,32 @@ class ModelManager:
         self.model.eval()
 
         while True:
-            eval_prompt = typer.prompt("\nuser ")
+            print("[bold yellow] user : [/bold yellow]")
+            eval_prompt = input("")
+
             if eval_prompt == "!bye":
                 self.unload_model()
                 break
             elif eval_prompt == "!history":
                 print(self.conversation_history)
+            elif eval_prompt =="!window":
+                window = self.conversation_history[-6:]
+                if window[0]["role"] == 'assistant':
+                    window = window[1:]
+                print(window)
             else:
                 self.conversation_history.append({"role": "user", "content": eval_prompt})
-                model_input = self.tokenizer.apply_chat_template(self.conversation_history[:5], return_tensors="pt").to("cuda")
+                window = self.conversation_history[-6:]
+                if window[0]["role"] == 'assistant':
+                    window = window[1:]
+
+                model_input = self.tokenizer.apply_chat_template(window, return_tensors="pt").to("cuda")
 
                 num_input_tokens = model_input.shape[1]
 
                 streamer = TextStreamer(self.tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+                print("[bold green]assistant : [/bold green]")
 
                 generated_tokens = self.model.generate(model_input, 
                                                     streamer=streamer, 
@@ -81,69 +103,87 @@ class ModelManager:
                 self.conversation_history.append({"role": "assistant", "content": assistant_response})
 
 
+def get_model_name_by_id(model_id: int) -> str:
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT name FROM llm_models WHERE id = ?', (model_id,))
+    row = cursor.fetchone()
+
+    conn.close()
+
+    if row:
+        return row[0]  # Return the model name
+    else:
+        raise ValueError
+
+
 @app.command()
 def run(base_model_id):
     global MODEL_MANAGER
-    MODEL_MANAGER = ModelManager(base_model_id)
+    name = get_model_name_by_id(base_model_id)
+    MODEL_MANAGER = ModelManager(name)
     MODEL_MANAGER.talk_to()
 
 
-# File path for the SQLite database
-db_file = 'my_database.db'
-
-# Function to create a connection to the SQLite database
 def get_db_connection():
     return sqlite3.connect(db_file)
 
-# Function to initialize the database
 def initialize_db():
     conn = get_db_connection()
     cursor = conn.cursor()
+
+    # Create the table only if it doesn't exist
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS llm_models (
-        name TEXT PRIMARY KEY,
-        additional_attribute TEXT
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL
     )
     ''')
     conn.commit()
     conn.close()
 
-@app.command()
-def add_model(name: str, additional_attribute: str = ''):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('INSERT OR REPLACE INTO llm_models (name, additional_attribute) VALUES (?, ?)', 
-                   (name, additional_attribute))
-    conn.commit()
-    conn.close()
-    typer.echo(f"Model added: {name} with attribute: {additional_attribute}")
 
 @app.command()
-def get_model(name: str):
+def add_model(name: str):
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute('SELECT name, additional_attribute FROM llm_models WHERE name = ?', (name,))
-    row = cursor.fetchone()
+    cursor.execute('INSERT INTO llm_models (name) VALUES (?)', 
+                   (name,))
+    conn.commit()
     conn.close()
-    if row:
-        typer.echo(f"llm_model: {row[0]}, Attribute: {row[1]}")
-    else:
-        typer.echo("Model not found.")
+    typer.echo(f"Model added: {name}")
+
+
 
 @app.command()
 def list_models():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute('SELECT name, additional_attribute FROM llm_models')
+    cursor.execute('SELECT id, name FROM llm_models')
     rows = cursor.fetchall()
     conn.close()
 
+    console = Console()
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("ID", style="dim", width=12)
+    table.add_column("Name", min_width=20)
+    table.add_column("Cached", justify="right")
+
     if rows:
-        typer.echo("List of LLM Models:")
         for row in rows:
-            typer.echo(f"Name: {row[0]}, Additional Attribute: {row[1]}")
+            filepath = try_to_load_from_cache(cache_dir=os.path.join(os.environ["HF_HOME"], "hub") , 
+                                              repo_id=row[1], 
+                                              filename="config.json")
+
+            cached_status = "Yes" if isinstance(filepath, str) else "No"
+            table.add_row(str(row[0]), row[1], cached_status)
     else:
-        typer.echo("No models found.")
+        console.print("No models found.")
+        return
+
+    console.print(table)
+
 
 if __name__ == "__main__":
     initialize_db()
@@ -151,5 +191,5 @@ if __name__ == "__main__":
     warnings.filterwarnings('ignore', category=FutureWarning)
 
     set_verbosity_error()
-    disable_progress_bar()
+    # disable_progress_bar()
     app()
