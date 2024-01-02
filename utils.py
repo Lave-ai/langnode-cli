7120ca8cfd7b461d76283d81056e2abc8a9d6639
Seungwoo Yoo <rufjdk5480@gmail.com>
2024-01-02 14:54:04 +0900

    add list_classes_in_module

--- utils.py
+++ utils.py
@@ -17,3 +17,27 @@ ascii_art = """
                               |             |
                                \___________/
 """
+
+import inspect
+import json
+import wrapper
+
+def list_classes_in_module():
+   class_list = []
+   for name, obj in inspect.getmembers(wrapper):
+      if inspect.isclass(obj) and obj.__module__ == wrapper.__name__:
+         class_list.append(obj)
+   to_dict = []
+   for c in class_list:
+      if hasattr(c, "definition"):
+         print(c)
+         print(c.definition())
+         to_dict.append(c.definition())
+
+   
+   with open("node_definitions.json", 'w') as f:
+      json.dump(to_dict,f,ensure_ascii=False,indent=2)
+
+
+if __name__ == "__main__":
+   list_classes_in_module()
